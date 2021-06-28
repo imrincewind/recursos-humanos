a4b777fa06134e19607fd62acd5cbf78725b087d
Ezequiel Fazio <ezfazio@gmail.com>
2021-06-28 13:47:18 -0300

    video 6 admin pages created and edited

---
+++ recursos-humanos/admin/sidebar.php
@@ -0,0 +1,25 @@
+        <!--/. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a class="active-menu" href="index.php"><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="tecnologias.php"><i class="fa fa-desktop"></i>Tecnologías</a>
+                    </li>
+					<li>
+                        <a href="recursos.php"><i class="fa fa-bar-chart-o"></i>Recursos</a>
+                    </li>
+                    <li>
+                        <a href="add-recurso.php"><i class="fa fa-qrcode"></i>Agregar Recurso</a>
+                    </li>
+
+                    
+                </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
